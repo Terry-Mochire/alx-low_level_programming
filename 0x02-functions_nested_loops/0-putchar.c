70ec7ee500d27d90e31340076e8f43209b6101d2
Terry Mochire <terrybmochire@gmail.com>
2022-03-15 14:14:49 -0700

    Final edit.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "stdio.h"
-/**
-* main - prints _putchar with a new line
-*
-* return: Always 0
-*
-*/
+#include "main.h"
 
+/**
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
 int main(void)
 {
+	char word[8] = "_putchar";
 	int i;
-	char p[] = "_putchar";
 
-		for (i = 0; i < 8; i++)
-		{
-			_putchar(p[i]);
-		}
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
 	_putchar('\n');
+
 	return (0);
 }
