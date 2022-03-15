1ea21f3828f3b2944bec73078afd41dff71cddd3
Terry Mochire <terrybmochire@gmail.com>
2022-03-15 11:53:12 -0700

    Edit 3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,12 +9,12 @@
 int  main(void)
 {
 	int i;
-	char p[] = "_putchar";
+	char p[] = "putchar";
 
 		for (i = 0; i < 9; i++)
 		{
-			_putchar(p[i]);
+			putchar(p[i]);
 		}
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
