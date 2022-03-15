ee70e5b188cc3e42ab01a8d0bac536a2cfe6b4e4
Terry Mochire <terrybmochire@gmail.com>
2022-03-15 09:21:32 -0700

    New repository.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>                                                                                                                                                      
+/**                                                                                                                                                                     
+* main - Prints _Putchar with a new line                                                                                                                                
+*                                                                                                                                                                       
+* Return: Always 0 (success)                                                                                                                                            
+*                                                                                                                                                                       
+*/                                                                                                                                                                      
+                                                                                                                                                                        
+int main(void)                                                                                                                                                          
+{                                                                                                                                                                       
+        int i; 
+	char p[] = "_putchar";
+
+		for(i=o; i < 9 ; i++)
+		{
+			putchar(p[i]);
+		}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# 0x02-functions_nested_loops
+This repository holds tasks for 0x02. C - Functions, nested loops
