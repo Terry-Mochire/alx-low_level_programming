28728136d36f1c36c417eb8af711fadcde430cbe
Terry Mochire <terrybmochire@gmail.com>
2022-03-15 10:40:58 -0700

    Edit betty style.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
-#include <stdio.h>                                                                                                                                                      
-/**                                                                                                                                                                     
-* main - Prints _Putchar with a new line                                                                                                                                
-*                                                                                                                                                                       
-* Return: Always 0 (success)                                                                                                                                            
-*                                                                                                                                                                       
-*/                                                                                                                                                                      
-                                                                                                                                                                        
-int main(void)                                                                                                                                                          
-{                                                                                                                                                                       
-        int i; 
+#include <stdio.h>
+/**
+* main - prints with a new line
+*
+* return: Always 0 (success)
+*
+*/
+
+int  main(void)
+{
+	int i;
 	char p[] = "_putchar";
 
-		for(i=0; i < 9 ; i++)
+		for (i = 0; i < 9; i++)
 		{
 			putchar(p[i]);
 		}
